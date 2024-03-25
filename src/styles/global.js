ae42f0181745401d3a9a51f9d748f8c59a21178b
David <dfl8@aluno.ifal.edu.br>
2024-03-25 15:45:05 -0300

    feat: add Roboto font

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,9 +7,21 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
+    :root {
+        font-size: 62.5%;
+    }
+
     body {
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
-        color: ${({theme}) => theme.COLORS.WHITE}
+        color: ${({theme}) => theme.COLORS.WHITE};
+
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button, textarea {
+        font-family: 'Roboto', serif;
+        font-size: 1.6rem;
+        outline: none;
     }
 
     a {

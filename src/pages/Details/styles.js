a54a06ba3f989a3cf908e0c43152efc7814a9cbb
David <dfl8@aluno.ifal.edu.br>
2024-04-17 00:48:33 -0300

    feat: add Note Component

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -12,7 +12,7 @@ export const Container = styled.div`
 
     > main {
         grid-area: content;
-        overflow-y: scroll;
+        overflow-y: auto;
         padding: 6.4rem 0;
     }
 `;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './style.js';
 import { FiPlus } from 'react-icons/fi';
 import { ButtonText } from '../../components/ButtonText/index.jsx';
 import { Header } from '../../components/Header/index.jsx';
-import { Container, Brand, Menu, Search, Content, NewNote } from './style.js';
 import { Input } from '../../components/Input/index.jsx';
+import { Section } from '../../components/Section/index.jsx';
+import { Note } from '../../components/Note/index.jsx';
 
 export function Home(){
     return(
@@ -26,7 +28,17 @@ export function Home(){
                 <Input placeholder="Pesquisar pelo título"/>
             </Search>
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{
+                        title: "React",
+                        tags: [
+                            {id: 1, name: "react"},
+                            {id: 2, name: "react"},
+                            {id: 3, name: "react"},
+                            {id: 4, name: "react"}
+                        ]
+                    }}/>
+                </Section>
             </Content>
             <NewNote>
                 <FiPlus/>

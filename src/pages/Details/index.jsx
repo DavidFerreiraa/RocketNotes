e7edb003000a35f7cdd671ec93f47c3db065ea2d
David <dfl8@aluno.ifal.edu.br>
2024-04-16 21:18:48 -0300

    feat: add button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,11 @@
 import { Container } from "./styles";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return(
     <Container>
       <h1>Hello world!</h1>
+      <Button title="My button" loading/>
     </Container>
   );
 }

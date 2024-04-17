6755b742ff14bef22e7bd57df9402d152bc329f2
David <dfl8@aluno.ifal.edu.br>
2024-04-16 22:10:58 -0300

    feat: add Tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return(
@@ -18,7 +19,8 @@ export function Details() {
         </Links>
       </Section>
       <Section title="Marcadores">
-        
+        <Tag title="Express"/>
+        <Tag title="Node"/>
       </Section>
       <Button title="My button" loading/>
     </Container>

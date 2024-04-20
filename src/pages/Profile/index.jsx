825c0a1ac4a2c05c36a8ea207cf80516a906d9e2
David <dfl8@aluno.ifal.edu.br>
2024-04-20 12:19:24 -0300

    feat: add profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile(){
     return(
@@ -14,6 +14,19 @@ export function Profile(){
                 </a>
             </header>
             <Form>
+                <Avatar>
+                    <img
+                        src="https://github.com/DavidFerreiraa.png"
+                        alt="Foto do usuário"
+                    />
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
                 <Input
                     placeholder="Nome"
                     type="text"
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -24,4 +24,52 @@ export const Form = styled.form`
     max-width: 34rem;
 
     margin: 3rem auto 0;
+
+    > div:nth-child(4) {
+        margin-top: 2.4rem
+    }
+`;
+
+export const Avatar = styled.div`
+    position: relative;
+    margin: -12.4rem auto 3.2rem;
+
+    width: 18.6rem;
+    height: 18.6rem;
+    
+    > img {
+        width: 18.6rem;
+        height: 18.6rem;
+
+        border-radius: 50%;
+    }
+
+    > label {
+        width: 4.8rem;
+        height: 4.8rem;
+
+        background-color: ${({ theme }) => theme.COLORS.ORANGE};
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 7px;
+        right: 7px;
+
+        cursor: pointer;
+
+        input {
+            display: none;
+        }
+
+        svg {
+            width: 2rem;
+            height: 2rem;
+
+            color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        }
+    }
 `;

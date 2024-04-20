c9745d6ac0b5058dd328179ffc29a14fe1410732
David <dfl8@aluno.ifal.edu.br>
2024-04-20 11:50:14 -0300

    feat: add SignIn page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,33 @@
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText'
+
+import { Background, Container, Form } from "./styles";
+
+export function SignIn(){
+    return(
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e grerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Button title="Entrar"/>
+                <ButtonText title="Criar conta"/>
+            </Form>
+            <Background/>
+        </Container>
+    );
+}
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import backgroundImg from '../../assets/background.png'
+
+export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    align-items: stretch;
+`;
+
+export const Form = styled.form`
+    padding: 0 13.6rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+
+    > h1 {
+        font-size: 4.8rem;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+
+    > h2 {
+        font-size: 2.4rem;
+        margin: 8.4rem 0 2.4rem 0;
+    }
+
+    > p {
+        font: 1.4rem;
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > button:last-child {
+        margin-top: 12.4rem;
+    }
+`;
+
+export const Background = styled.div`
+    flex: 1;
+    background: url(${backgroundImg}) no-repeat center center;
+    background-size: cover;
+    filter: opacity(0.2);
+`;

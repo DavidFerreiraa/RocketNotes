20a8025cc6bf190186858e2e2272c492859d51b2
David <dfl8@aluno.ifal.edu.br>
2024-04-20 11:58:59 -0300

    feat: add SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -6,14 +6,20 @@ import { ButtonText } from '../../components/ButtonText'
 
 import { Background, Container, Form } from "./styles";
 
-export function SignIn(){
+export function SignUp(){
     return(
         <Container>
+            <Background/>
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e grerenciar seus links úteis.</p>
 
-                <h2>Faça seu login</h2>
+                <h2>Crie sua conta</h2>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
                 <Input
                     placeholder="E-mail"
                     type="text"
@@ -24,10 +30,9 @@ export function SignIn(){
                     type="password"
                     icon={FiLock}
                 />
-                <Button title="Entrar"/>
-                <ButtonText title="Criar conta"/>
+                <Button title="Cadastrar"/>
+                <ButtonText title="Voltar para o login"/>
             </Form>
-            <Background/>
         </Container>
     );
 }

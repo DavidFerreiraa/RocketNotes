45f5973e375e08f03a659bebf910d78d288d1762
David <dfl8@aluno.ifal.edu.br>
2024-04-22 22:53:36 -0300

    feat: add handleSignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { FiUser, FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { ButtonText } from '../../components/ButtonText'
+import { ToastContainer, toast } from "react-toastify";
 
 import { Background, Container, Form } from "./styles";
+import { api } from "../../services/api";
+import 'react-toastify/dist/ReactToastify.css';
 
 export function SignUp(){
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    function handleSignUp(){
+        if(!name || !email || !password) {
+            return toast.warn("Preencha todos os campos!");
+        }
+
+        api.post("/users", { name, email, password }, )
+            .then(() => toast.success("Usuário cadastrado com sucesso!"))
+            .catch(error => {
+                if (error.response) {
+                    return toast.error(error.response.data.message);
+                } else {
+                    console.log(error);
+                    return toast.error("Não foi possível cadastrar o usuário.");
+                }
+            })
+    }
+
     return(
         <Container>
             <Background/>
@@ -20,20 +44,24 @@ export function SignUp(){
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
                 <Input
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignUp} />
                 <Link to="/">Voltar para o login</Link>
             </Form>
+            <ToastContainer />
         </Container>
     );
 }

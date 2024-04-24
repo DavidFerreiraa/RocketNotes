0cd1587da00ee8e3d654fcb1198c0cd8cdff88ee
David <dfl8@aluno.ifal.edu.br>
2024-04-24 00:08:45 -0300

    feat: add handleNewNote

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
@@ -8,14 +8,22 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 
 import { Container, Form } from './styles';
-import { Link } from 'react-router-dom';
+import { api } from '../../services/api';
+import { toast } from 'react-toastify';
+
+import { useState } from 'react';
 
 export function New(){
+    const [ title, setTitle ] = useState("");
+    const [ description, setDescription ] = useState("");
+
     const [ links, setLinks ] = useState([]);
     const [ newLink, setNewLink ] = useState("");
     
     const [ tags, setTags ] = useState([]);
     const [ newTag, setNewTag ] = useState("");
+
+    const navigate = useNavigate();
 
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink]);
@@ -35,6 +43,18 @@ export function New(){
         setTags(prevState => prevState.filter(tag => tag !== deleted)); //return only tags differents of the deleted
     }
 
+    async function handleNewNote(){
+        await api.post("/notes", {
+            title,
+            description,
+            tags,
+            links
+        })
+
+        toast.success("Nota criada com sucesso!");
+        navigate("/");
+    }
+
     return(
         <Container>
             <Header/>
@@ -47,10 +67,12 @@ export function New(){
 
                     <Input
                         placeholder="Título"
+                        onChange={e => setTitle(e.target.value)}
                     />
 
                     <Textarea
                         placeholder="Observações"
+                        onChange ={e => setDescription(e.target.value)}
                     />
 
                     <Section title="Link úteis">
@@ -75,8 +97,12 @@ export function New(){
                     <Section title="Marcadores">
                         <div className="tags">
                             {
-                                tags.map((tag) => (
-                                    <NoteItem value={tag} onClick={() => handleRemoveTag(tag)}/>
+                                tags.map((tag, index) => (
+                                    <NoteItem 
+                                        key={String(index)}
+                                        value={tag} 
+                                        onClick={() => handleRemoveTag(tag)}
+                                    />
                                 ))
                             }
                             <NoteItem 
@@ -89,7 +115,10 @@ export function New(){
                         </div>
                     </Section>
 
-                    <Button title="Salvar"/>
+                    <Button 
+                        title="Salvar" 
+                        onClick={handleNewNote}
+                    />
                 </Form>
             </main>
         </Container>

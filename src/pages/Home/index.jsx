361a2aa2aad67295df2e2e1a203166375c463ff6
David <dfl8@aluno.ifal.edu.br>
2024-04-24 00:55:31 -0300

    feat: add fetchTags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,28 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from './style.js';
+
 import { ButtonText } from '../../components/ButtonText/index.jsx';
 import { Header } from '../../components/Header/index.jsx';
 import { Input } from '../../components/Input/index.jsx';
 import { Section } from '../../components/Section/index.jsx';
 import { Note } from '../../components/Note/index.jsx';
+
+import { useEffect, useState } from 'react';
+
 import { FiPlus } from 'react-icons/fi';
+import { api } from '../../services/api.js';
 
 export function Home(){
+    const [tags, setTags] = useState([]);
+
+    
+    useEffect(() => {
+        async function fetchTags(){
+            const response = await api.get("/tags");
+            setTags(response.data);
+        }
+
+        fetchTags();
+    }, [])
     return(
         <Container>
             <Brand>
@@ -17,12 +33,17 @@ export function Home(){
                 <li>
                     <ButtonText title="Todos" isActive />
                 </li>
-                <li>
-                    <ButtonText title="React"/>
-                </li>
-                <li>
-                    <ButtonText title="Node"/>
-                </li>
+                {
+                tags && tags.map(tag => (
+                    <li>
+                        <ButtonText 
+                            key={String(tag.id)}
+                            title={tag.name} 
+                        />
+                    </li>
+                ))
+                }
+                
             </Menu>
             <Search>
                 <Input placeholder="Pesquisar pelo título"/>

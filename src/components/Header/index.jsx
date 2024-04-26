6c34c83ef3dcc1aa52c36fef45d421616f814806
David <dfl8@aluno.ifal.edu.br>
2024-04-25 23:16:24 -0300

    fix: fix the user name catch

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -27,7 +27,7 @@ export function Header(){
                 />
                 <div>
                     <span>Bem-vindo</span>
-                    <strong>David Ferreira</strong>
+                    <strong>{user.name}</strong>
                 </div>
             </Profile>
             <Logout onClick={handleSignOut}>
